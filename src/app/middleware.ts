636c60096d8e74e5fa7dbf22cf4f3503694cf8ab
Arpit Gautam <arpitgautam054@gmail.com>
2026-07-12 23:27:55 -1200

    Simplified middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,19 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Yahan define karein ki kaunse pages public rahenge
-// Abhi ke liye sirf sign-in aur sign-up ko public rakha hai
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
-
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect(); // Ye line user ko login page par dhakel degi agar wo login nahi hai
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    '/((?!.*\\..*|_next).*)', 
-    '/', 
+    '/((?!.*\\..*|_next).*)',
+    '/',
     '/(api|trpc)(.*)',
   ],
 };
