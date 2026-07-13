3dd4ebc7c9f388adf583618230febe002511a7ff
Arpit Gautam <arpitgautam054@gmail.com>
2026-07-13 02:09:43 -1200

    fix middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -4,17 +4,14 @@ const isProtectedRoute = createRouteMatcher(['/(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    const session = await auth();
-    if (!session.userId) {
-       auth().redirectToSignIn();
-    }
+    // Ye line bina error ke kaam karegi
+    await auth.protect();
   }
 });
 
 export const config = {
   matcher: [
-    '/((?!.*\\..*|_next).*)',
-    '/',
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
 };
